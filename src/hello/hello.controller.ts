d140d74d283a18a9286c61f51dcae1628414343a
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-01-19 20:17:23 -0300

    guards

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -8,9 +8,11 @@ import {
   Query,
   Req,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 @Controller()
 export class HelloController {
@@ -45,12 +47,14 @@ export class HelloController {
   }
 
   @Get('/active/:status')
+  @UseGuards(AuthGuard)
   isUserActive(@Param('status', ParseBoolPipe) status: boolean) {
     console.log(typeof status);
     return status;
   }
 
   @Get('/greet')
+  @UseGuards(AuthGuard)
   greet(@Query(ValidateuserPipe) query: { name: string; age: number }) {
     console.log(typeof query.name);
     console.log(typeof query.age);
